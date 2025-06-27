b7697fa98f8a940f29edf9ca90784cd90485d984
Subratp12 <subrat.padhi1204@outlook.com>
2025-06-27 12:59:25 +0530

    Update config.js

--- client/src/pages/config.js
+++ client/src/pages/config.js
@@ -1,5 +1,5 @@
 // const API_BASE_URL = "http://3.87.228.149:84";
-const API_BASE_URL = "http://20.195.14.65";
+const API_BASE_URL = "http://57.155.2.70";
 // export default API_BASE_URL;
 // const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://backend";
 /// export default API_BASE_URL;
